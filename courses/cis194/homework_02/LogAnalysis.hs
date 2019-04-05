4a9b32dae4e625b97602298027bfe62bf2d16b1b
Jose Reyna <jose.reyna.rb@gmail.com>
2019-04-05 10:23:22 -0400

    Initial work on cis194 homework 2

---
+++ courses/cis194/homework_02/LogAnalysis.hs
@@ -0,0 +1,50 @@
+-- https://stackoverflow.com/questions/11022163/haskell-read-lines-of-file
+-- https://stackoverflow.com/questions/4978578/how-to-split-a-string-in-haskell
+-- https://stackoverflow.com/questions/5289779/printing-elements-of-a-list-on-new-lines
+
+module LogAnalysis where
+
+import System.Environment -- getArgs
+import Log
+import Data.List.Split -- splitOn, with cabal install split
+import Data.List -- intercalate function
+
+parseInformationMessage :: String -> LogMessage
+parseInformationMessage s = do
+    let elements = splitOn " " s
+    let timestamp = (read (elements !! 1) :: Int) 
+    let string = intercalate " " (tail (tail elements))
+    LogMessage Info timestamp string
+
+parseErrorMessage :: String -> LogMessage
+parseErrorMessage s = do
+    let elements = splitOn " " s
+    let errorlevel = (read (elements !! 1) :: Int)
+    let error = Error errorlevel 
+    let timestamp = (read (elements !! 2) :: Int) 
+    let string = intercalate " " (tail (tail (tail elements)))
+    LogMessage error timestamp string
+
+parseWarningMessage :: String -> LogMessage
+parseWarningMessage s = do
+    let elements = splitOn " " s
+    let timestamp = (read (elements !! 1) :: Int) 
+    let string = intercalate " " (tail (tail elements))
+    LogMessage Warning timestamp string
+
+parseMessage :: String -> LogMessage
+parseMessage s = do
+    let elements = splitOn " " s -- first item after splitting by spaces (message type)
+    -- let key = (intercalate "" (head elements))   does not work
+    let key = elements !! 0
+    case key of 
+        "I" -> parseInformationMessage s
+        "W" -> parseWarningMessage s
+        "E" -> parseErrorMessage s
+        _  -> Unknown s
+
+main = do
+    args <- getArgs
+    content <- readFile (args !! 0) -- takes the first element (at position 0)
+    let messages = map parseMessage (lines content)
+    mapM_ print messages
