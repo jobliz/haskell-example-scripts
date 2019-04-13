ab7afe061474657639498c0f90a8c85218012d85
Jose Reyna <jose.reyna.rb@gmail.com>
2019-04-13 10:20:01 -0400

    Parsec calculator example from the web

---
+++ packages/parsec/calculator_01.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+-- Code originally from:
+-- https://github.com/hughfdjackson/calculator/blob/master/src/Main.hs
+-- Requires the LANGUAGE instruction to run in current Haskell, it seems.
+-- Runs, but TODO: Requires an evaluation function.
+
+import Prelude hiding (subtract)
+import Text.Parsec
+import Text.Parsec.String (Parser)
+
+number :: Parser Float
+number = fmap readFloat numberString
+  where readFloat = read :: String -> Float
+        numberString = many1 digit
+
+plus :: Parser (Float -> Float -> Float)
+plus = fmap (const add) plusChar
+  where add a b = a + b
+        plusChar = char '+'
+
+subtract :: Parser (Float -> Float -> Float)
+subtract = fmap (const subtract) subtractChar
+  where subtract a b = a - b
+        subtractChar = char '-'
+
+divide :: Parser (Float -> Float -> Float)
+divide = fmap (const divide) divideChar
+  where divide a b = a / b
+        divideChar = char '/'
+
+multiply :: Parser (Float -> Float -> Float)
+multiply = fmap (const multiply) multiplyChar
+  where multiply a b = a * b
+        multiplyChar = char '*'
+
+expressionWithParens :: Parser Float
+expressionWithParens = between (char '(') (char ')') expression
+
+expressionWithParensOrNumber :: Parser Float
+expressionWithParensOrNumber = withWhitespace (expressionWithParens <|> number)
+  where withWhitespace parser = between spaces spaces parser 
+
+chainMultiplyDivide :: Parser Float
+chainMultiplyDivide = chainl1 expressionWithParensOrNumber (divide <|> multiply) 
+
+expression :: Parser Float
+expression = chainl1 chainMultiplyDivide (plus <|> subtract) 
+
+main = print "Hello"
